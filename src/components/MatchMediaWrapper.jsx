7780c8eef4953a04ecad15ffd5317c91417b3c87
Waris Aremou <warisaremou2003@gmail.com>
2022-10-22 03:57:02 +0100

    Mise en page du front de l'app

---
+++ src/components/MatchMediaWrapper.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+function MatchMediaWrapper({mobileContent, desktopContent}) {
+    const [isNarrowScreen, setIsNarrowScreen] = useState(false)
+    useEffect(() => {
+      const mediaWatcher = window.matchMedia("(max-width: 768px)")
+      setIsNarrowScreen(mediaWatcher.matches);
+  
+      function updateIsNarrowScreen(e) {
+        setIsNarrowScreen(e.matches);
+      }
+      if(mediaWatcher.addEventListener) {
+        mediaWatcher.addEventListener('change', updateIsNarrowScreen)
+        return function cleanup() {
+          mediaWatcher.removeEventListener('change', updateIsNarrowScreen)
+        }
+      } else {
+        mediaWatcher.addListener(updateIsNarrowScreen)
+        return function cleanup() {
+          mediaWatcher.removeListener(updateIsNarrowScreen)
+        }
+      }
+    }, [])
+  
+    return isNarrowScreen ? mobileContent : desktopContent
+}
+
+export default MatchMediaWrapper
