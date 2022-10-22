bb235707160958aca421a9219b32070be9eba4ec
Waris Aremou <warisaremou2003@gmail.com>
2022-10-22 13:11:30 +0100

    Récupération des des données de l'api et ajout d'un loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import MatchMediaWrapper from "./MatchMediaWrapper";
 import "../style/App.css";
 
 function App() {
+  const [adviceDetails, setAdviceDetails] = useState({});
+  const [loader, setLoader] = useState(false);
+  const api = "https://api.adviceslip.com/advice";
+
   const mobileContent = (
     <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
       <g fill="none" fillRule="evenodd">
@@ -25,18 +30,40 @@ function App() {
     </svg>
   );
 
+  async function generateAdvice() {
+    setLoader(true);
+
+    fetch(api)
+      .then((response) => response.json())
+      .then((value) => setAdviceDetails(value))
+      .catch((error) => console.log(error.message));
+
+    setLoader(false);
+  }
+
   return (
     <div className="wrapper">
-      <h4 className="advice-id">Advice #117</h4>
+      <h4 className="advice-id">
+        Advice {`#${adviceDetails.slip ? adviceDetails.slip.id : 0}`}
+      </h4>
       <p className="advice">
-        "It is easy to sit up and take notice, what's difficult is getting up
-        and taking action."
+        {adviceDetails.slip
+          ? adviceDetails.slip.advice
+          : "Click to generate random advice"}
       </p>
+      {loader ? (
+        <div className={"loader"}>LOADING</div>
+      ) : null}
       <MatchMediaWrapper
         mobileContent={mobileContent}
         desktopContent={desktopContent}
       />
-      <p className="icon-dice">
+      <p
+        className="icon-dice"
+        onClick={() => {
+          generateAdvice();
+        }}
+      >
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
